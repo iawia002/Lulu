0e4e380a9d531fa2b8069cbb1779920d37782381
iawia002 <z2d@jifangcheng.com>
2018-01-17 17:21:07 +0800

    🔧 extractor.py

--- lulu/extractor.py
+++ lulu/extractor.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 
-from .common import match1, maybe_print, download_urls, get_filename, parse_host, set_proxy, unset_proxy, get_content, dry_run
-from .common import print_more_compatible as print
-from .util import log
-from . import json_output
 import os
 
-class Extractor():
+from lulu.common import (
+    dry_run,
+    set_proxy,
+    parse_host,
+    unset_proxy,
+    maybe_print,
+    get_filename,
+    download_urls,
+    print_more_compatible as print,
+)
+from lulu.util import log
+from lulu import json_output
+
+
+class Extractor:
     def __init__(self, *args):
         self.url = None
         self.title = None
@@ -17,7 +27,8 @@ class Extractor():
         if args:
             self.url = args[0]
 
-class VideoExtractor():
+
+class VideoExtractor:
     def __init__(self, *args):
         self.url = None
         self.title = None
@@ -50,9 +61,19 @@ class VideoExtractor():
             unset_proxy()
 
         try:
-            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
-        except:
-            self.streams_sorted = [dict([('itag', stream_type['itag'])] + list(self.streams[stream_type['itag']].items())) for stream_type in self.__class__.stream_types if stream_type['itag'] in self.streams]
+            self.streams_sorted = [
+                dict([('id', stream_type['id'])] + list(
+                    self.streams[stream_type['id']].items()
+                )) for stream_type in self.__class__.stream_types
+                if stream_type['id'] in self.streams
+            ]
+        except Exception:
+            self.streams_sorted = [
+                dict([('itag', stream_type['itag'])] + list(
+                    self.streams[stream_type['itag']].items()
+                )) for stream_type in self.__class__.stream_types
+                if stream_type['itag'] in self.streams
+            ]
 
         self.extract(**kwargs)
 
@@ -69,9 +90,19 @@ class VideoExtractor():
             unset_proxy()
 
         try:
-            self.streams_sorted = [dict([('id', stream_type['id'])] + list(self.streams[stream_type['id']].items())) for stream_type in self.__class__.stream_types if stream_type['id'] in self.streams]
-        except:
-            self.streams_sorted = [dict([('itag', stream_type['itag'])] + list(self.streams[stream_type['itag']].items())) for stream_type in self.__class__.stream_types if stream_type['itag'] in self.streams]
+            self.streams_sorted = [
+                dict([('id', stream_type['id'])] + list(
+                    self.streams[stream_type['id']].items()
+                )) for stream_type in self.__class__.stream_types
+                if stream_type['id'] in self.streams
+            ]
+        except Exception:
+            self.streams_sorted = [
+                dict([('itag', stream_type['itag'])] + list(
+                    self.streams[stream_type['itag']].items()
+                )) for stream_type in self.__class__.stream_types
+                if stream_type['itag'] in self.streams
+            ]
 
         self.extract(**kwargs)
 
@@ -79,11 +110,11 @@ class VideoExtractor():
 
     def prepare(self, **kwargs):
         pass
-        #raise NotImplementedError()
+        # raise NotImplementedError()
 
     def extract(self, **kwargs):
         pass
-        #raise NotImplementedError()
+        # raise NotImplementedError()
 
     def p_stream(self, stream_id):
         if stream_id in self.streams:
@@ -92,9 +123,13 @@ class VideoExtractor():
             stream = self.dash_streams[stream_id]
 
         if 'itag' in stream:
-            print("    - itag:          %s" % log.sprint(stream_id, log.NEGATIVE))
+            print("    - itag:          %s" % log.sprint(
+                stream_id, log.NEGATIVE
+            ))
         else:
-            print("    - format:        %s" % log.sprint(stream_id, log.NEGATIVE))
+            print("    - format:        %s" % log.sprint(
+                stream_id, log.NEGATIVE
+            ))
 
         if 'container' in stream:
             print("      container:     %s" % stream['container'])
@@ -106,16 +141,22 @@ class VideoExtractor():
             print("      quality:       %s" % stream['quality'])
 
         if 'size' in stream and stream['container'].lower() != 'm3u8':
-            if stream['size'] != float('inf')  and stream['size'] != 0:
-                print("      size:          %s MiB (%s bytes)" % (round(stream['size'] / 1048576, 1), stream['size']))
+            if stream['size'] != float('inf') and stream['size'] != 0:
+                print("      size:          {} MiB ({} bytes)".format(
+                    round(stream['size'] / 1048576, 1), stream['size']
+                ))
 
         if 'm3u8_url' in stream:
             print("      m3u8_url:      {}".format(stream['m3u8_url']))
 
         if 'itag' in stream:
-            print("    # download-with: %s" % log.sprint("lulu --itag=%s [URL]" % stream_id, log.UNDERLINE))
+            print("    # download-with: %s" % log.sprint(
+                "lulu --itag=%s [URL]" % stream_id, log.UNDERLINE
+            ))
         else:
-            print("    # download-with: %s" % log.sprint("lulu --format=%s [URL]" % stream_id, log.UNDERLINE))
+            print("    # download-with: %s" % log.sprint(
+                "lulu --format=%s [URL]" % stream_id, log.UNDERLINE
+            ))
 
         print()
 
@@ -126,7 +167,9 @@ class VideoExtractor():
             stream = self.dash_streams[stream_id]
 
         maybe_print("    - title:         %s" % self.title)
-        print("       size:         %s MiB (%s bytes)" % (round(stream['size'] / 1048576, 1), stream['size']))
+        print("       size:         {} MiB ({} bytes)".format(
+            round(stream['size'] / 1048576, 1), stream['size']
+        ))
         print("        url:         %s" % self.url)
         print()
 
@@ -141,7 +184,9 @@ class VideoExtractor():
         elif stream_id is None:
             # Print stream with best quality
             print("stream:              # Best quality")
-            stream_id = self.streams_sorted[0]['id'] if 'id' in self.streams_sorted[0] else self.streams_sorted[0]['itag']
+            stream_id = self.streams_sorted[0]['id'] \
+                if 'id' in self.streams_sorted[0] \
+                else self.streams_sorted[0]['itag']
             self.p_stream(stream_id)
 
         elif stream_id == []:
@@ -149,14 +194,18 @@ class VideoExtractor():
             # Print DASH streams
             if self.dash_streams:
                 print("    [ DASH ] %s" % ('_' * 36))
-                itags = sorted(self.dash_streams,
-                               key=lambda i: -self.dash_streams[i]['size'])
+                itags = sorted(
+                    self.dash_streams,
+                    key=lambda i: -self.dash_streams[i]['size']
+                )
                 for stream in itags:
                     self.p_stream(stream)
             # Print all other available streams
             print("    [ DEFAULT ] %s" % ('_' * 33))
             for stream in self.streams_sorted:
-                self.p_stream(stream['id'] if 'id' in stream else stream['itag'])
+                self.p_stream(
+                    stream['id'] if 'id' in stream else stream['itag']
+                )
 
         if self.audiolang:
             print("audio-languages:")
@@ -185,7 +234,9 @@ class VideoExtractor():
                 if 'index' not in kwargs:
                     self.p([])
                 else:
-                    stream_id = self.streams_sorted[0]['id'] if 'id' in self.streams_sorted[0] else self.streams_sorted[0]['itag']
+                    stream_id = self.streams_sorted[0]['id'] \
+                        if 'id' in self.streams_sorted[0] \
+                        else self.streams_sorted[0]['itag']
                     self.p_i(stream_id)
 
         else:
@@ -194,8 +245,9 @@ class VideoExtractor():
                 stream_id = kwargs['stream_id']
             else:
                 # Download stream with the best quality
-                from .processor.ffmpeg import has_ffmpeg_installed
-                stream_id = self.streams_sorted[0]['id'] if 'id' in self.streams_sorted[0] else self.streams_sorted[0]['itag']
+                stream_id = self.streams_sorted[0]['id'] \
+                    if 'id' in self.streams_sorted[0] \
+                    else self.streams_sorted[0]['itag']
 
             if 'index' not in kwargs:
                 self.p(stream_id)
@@ -222,10 +274,16 @@ class VideoExtractor():
                 headers['User-Agent'] = self.ua
             if self.referer is not None:
                 headers['Referer'] = self.referer
-            download_urls(urls, self.title, ext, total_size, headers=headers,
-                          output_dir=kwargs['output_dir'],
-                          merge=kwargs['merge'],
-                          av=stream_id in self.dash_streams)
+            download_urls(
+                urls,
+                self.title,
+                ext,
+                total_size,
+                headers=headers,
+                output_dir=kwargs['output_dir'],
+                merge=kwargs['merge'],
+                av=stream_id in self.dash_streams
+            )
             if 'caption' not in kwargs or not kwargs['caption']:
                 print('Skipping captions or danmuku.')
                 return
@@ -233,18 +291,32 @@ class VideoExtractor():
                 filename = '%s.%s.srt' % (get_filename(self.title), lang)
                 print('Saving %s ... ' % filename, end="", flush=True)
                 srt = self.caption_tracks[lang]
-                with open(os.path.join(kwargs['output_dir'], filename),
-                          'w', encoding='utf-8') as x:
+                with open(
+                    os.path.join(kwargs['output_dir'], filename),
+                    'w',
+                    encoding='utf-8'
+                ) as x:
                     x.write(srt)
                 print('Done.')
             if self.danmuku is not None and not dry_run:
                 filename = '{}.cmt.xml'.format(get_filename(self.title))
                 print('Downloading {} ...\n'.format(filename))
-                with open(os.path.join(kwargs['output_dir'], filename), 'w', encoding='utf8') as fp:
+                with open(
+                    os.path.join(kwargs['output_dir'], filename),
+                    'w',
+                    encoding='utf8'
+                ) as fp:
                     fp.write(self.danmuku)
 
             # For main_dev()
-            #download_urls(urls, self.title, self.streams[stream_id]['container'], self.streams[stream_id]['size'])
+            # download_urls(
+            #     urls, self.title, self.streams[stream_id]['container'],
+            #     self.streams[stream_id]['size']
+            # )
         keep_obj = kwargs.get('keep_obj', False)
         if not keep_obj:
             self.__init__()
+
+
+class SimpleExtractor:
+    pass
