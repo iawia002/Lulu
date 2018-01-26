751e4b26be2fa5088f0ee3fc037775ecbc97b6be
iawia002 <z2d@jifangcheng.com>
2018-01-26 16:17:35 +0800

    doc [ci skip]

--- tests/util.py
+++ tests/util.py
@@ -5,8 +5,9 @@ import unittest
 from urllib.error import URLError
 
 
+# 在 CI 上百分百失败用下面的 skipIf，有可能失败的用 ignore_network_issue
+# 使用 skip 或者 ignore 的必须在本地上跑通过
 NETWORK_ISSUE = 'tests will fail due to the network issue'
-
 skipOnCI = unittest.skipIf('CI' in os.environ, NETWORK_ISSUE)
 skipOnAppVeyor = unittest.skipIf('APPVEYOR' in os.environ, NETWORK_ISSUE)
 skipOnTravis = unittest.skipIf('TRAVIS' in os.environ, NETWORK_ISSUE)
