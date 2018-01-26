770c263857279134ded8c56b4162a8bbb8b249f9
iawia002 <z2d@jifangcheng.com>
2018-01-26 14:41:42 +0800

    ⬆️ .travis.yml [skip appveyor]

--- tests/util.py
+++ tests/util.py
@@ -8,3 +8,4 @@ NETWORK_ISSUE = 'tests will fail due to the network issue'
 
 skipOnCI = unittest.skipIf('CI' in os.environ, NETWORK_ISSUE)
 skipOnAppVeyor = unittest.skipIf('APPVEYOR' in os.environ, NETWORK_ISSUE)
+skipOnTravis = unittest.skipIf('TRAVIS' in os.environ, NETWORK_ISSUE)
