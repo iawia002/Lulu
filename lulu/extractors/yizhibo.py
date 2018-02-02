4fc882ceea50fa86db0c2275195ad8a36287532e
iawia002 <z2d@jifangcheng.com>
2018-02-02 13:13:11 +0800

    extractors/yizhibo: code format

--- lulu/extractors/yizhibo.py
+++ lulu/extractors/yizhibo.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python
 
-__all__ = ['yizhibo_download']
-
-from ..common import *
+import re
 import json
-import time
 
-def yizhibo_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
-    video_id = url[url.rfind('/')+1:].split(".")[0]
-    json_request_url = 'http://www.yizhibo.com/live/h5api/get_basic_live_info?scid={}'.format(video_id)
+from lulu.common import (
+    player,
+    print_info,
+    get_content,
+    download_urls,
+    launch_player,
+    playlist_not_supported,
+)
+
+
+__all__ = ['yizhibo_download']
+site_info = 'yizhibo.com'
+
+
+def yizhibo_download(
+    url, output_dir='.', merge=True, info_only=False, **kwargs
+):
+    video_id = url[url.rfind('/')+1:].split('.')[0]
+    json_request_url = (
+        'http://www.yizhibo.com/live/h5api/get_basic_live_info?'
+        'scid={}'.format(video_id)
+    )
     content = get_content(json_request_url)
     error = json.loads(content)['result']
-    if (error != 1):
-        raise ValueError("Error : {}".format(error))
+    if error != 1:
+        raise ValueError('Error : {}'.format(error))
 
     data = json.loads(content)
     title = data.get('data')['live_title']
@@ -20,7 +36,9 @@ def yizhibo_download(url, output_dir = '.', merge = True, info_only = False, **k
         title = data.get('data')['nickname']
     m3u8_url = data.get('data')['play_url']
     m3u8 = get_content(m3u8_url)
-    base_url = "/".join(data.get('data')['play_url'].split("/")[:7])+"/"
+    base_url = '{}/'.format(
+        '/'.join(data.get('data')['play_url'].split('/')[:7])
+    )
     part_url = re.findall(r'([0-9]+\.ts)', m3u8)
     real_url = []
     for i in part_url:
@@ -30,8 +48,10 @@ def yizhibo_download(url, output_dir = '.', merge = True, info_only = False, **k
     if not info_only:
         if player:
             launch_player(player, [m3u8_url])
-        download_urls(real_url, title, 'ts', float('inf'), output_dir, merge = merge)
+        download_urls(
+            real_url, title, 'ts', float('inf'), output_dir, merge=merge
+        )
 
-site_info = "yizhibo.com"
+
 download = yizhibo_download
-download_playlist = playlist_not_supported('yizhibo')
+download_playlist = playlist_not_supported(site_info)
