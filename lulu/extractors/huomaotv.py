d42afdd8788c4c7a18b2daf89253cb8e532c2f1e
iawia002 <z2d@jifangcheng.com>
2018-02-07 16:35:08 +0800

    extractors/huomaotv: 🔧

--- lulu/extractors/huomaotv.py
+++ lulu/extractors/huomaotv.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 
+from lulu.common import (
+    match1,
+    print_info,
+    get_content,
+    download_url_ffmpeg,
+    playlist_not_supported,
+)
+from lulu.util.parser import get_parser
+
+
 __all__ = ['huomaotv_download']
-
-from ..common import *
-
-
-def get_mobile_room_url(room_id):
-    return 'http://www.huomao.com/mobile/mob_live/%s' % room_id
+site_info = '火猫直播 huomao.com'
 
 
-def get_m3u8_url(stream_id):
-    return 'http://live-ws.huomaotv.cn/live/%s/playlist.m3u8' % stream_id
-
-
-def huomaotv_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+def huomaotv_download(
+    url, output_dir='.', merge=True, info_only=False, **kwargs
+):
     room_id_pattern = r'huomao.com/(\d+)'
     room_id = match1(url, room_id_pattern)
-    html = get_content(get_mobile_room_url(room_id))
-
-    stream_id_pattern = r'id="html_stream" value="(\w+)"'
-    stream_id = match1(html, stream_id_pattern)
-
-    m3u8_url = get_m3u8_url(stream_id)
-
-    title = match1(html, r'<title>([^<]{1,9999})</title>')
+    html = get_content(
+        'http://m.huomao.com/mobile/mob_live/{}'.format(room_id)
+    )
+    parser = get_parser(html)
+    m3u8_url = parser.source['src']
+    title = parser.title.text
 
     print_info(site_info, title, 'm3u8', float('inf'))
 
     if not info_only:
-        download_url_ffmpeg(m3u8_url, title, 'm3u8', None, output_dir=output_dir, merge=merge)
+        download_url_ffmpeg(
+            m3u8_url, title, 'mp4', None, output_dir=output_dir, merge=merge
+        )
 
 
-site_info = 'huomao.com'
 download = huomaotv_download
-download_playlist = playlist_not_supported('huomao')
+download_playlist = playlist_not_supported(site_info)
