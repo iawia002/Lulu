2a5abfce8235d8fdc039c5f66012035e42e79791
iawia002 <z2d@jifangcheng.com>
2018-02-11 15:27:35 +0800

    extractors/quanmin: code format

--- lulu/extractors/quanmin.py
+++ lulu/extractors/quanmin.py
@@ -1,28 +1,38 @@
 #!/usr/bin/env python
 
-__all__ = ['quanmin_download']
-
-from ..common import *
 import json
-import time
 
-def quanmin_download(url, output_dir = '.', merge = True, info_only = False, **kwargs):
+from lulu.common import (
+    print_info,
+    get_content,
+    download_urls,
+    playlist_not_supported,
+)
+
+
+__all__ = ['quanmin_download']
+site_info = '全民直播 quanmin.tv'
+
+
+def quanmin_download(url, info_only=False, **kwargs):
     roomid = url.split('/')[3].split('?')[0]
 
-    json_request_url = 'http://m.quanmin.tv/json/rooms/{}/noinfo6.json'.format(roomid)
-    content = get_html(json_request_url)
+    json_request_url = (
+        'https://m.quanmin.tv/json/rooms/{}/noinfo6.json'.format(roomid)
+    )
+    content = get_content(json_request_url)
     data = json.loads(content)
 
-    title = data["title"]
+    title = data['title']
 
-    if not data["play_status"]:
-        raise ValueError("The live stream is not online!")
-    real_url = "http://flv.quanmin.tv/live/{}.flv".format(roomid)
+    if not data['play_status']:
+        raise ValueError('The live stream is not online!')
+    real_url = 'https://flv.quanmin.tv/live/{}.flv'.format(roomid)
 
     print_info(site_info, title, 'flv', float('inf'))
     if not info_only:
-        download_urls([real_url], title, 'flv', None, output_dir, merge = merge)
+        download_urls([real_url], title, 'flv', None, **kwargs)
 
-site_info = "quanmin.tv"
+
 download = quanmin_download
-download_playlist = playlist_not_supported('quanmin')
+download_playlist = playlist_not_supported(site_info)
