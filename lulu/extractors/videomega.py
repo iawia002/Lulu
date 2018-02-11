50d176c598975da0ff428942c7a5e28211c5453e
iawia002 <z2d@jifangcheng.com>
2018-02-11 16:46:23 +0800

    extractors/videomega: code format

--- lulu/extractors/videomega.py
+++ lulu/extractors/videomega.py
@@ -1,28 +1,47 @@
 #!/usr/bin/env python
 
-__all__ = ['videomega_download']
-
-from ..common import *
+import re
 import ssl
+from urllib import request
 
-def videomega_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
+from lulu.common import (
+    match1,
+    url_info,
+    print_info,
+    get_content,
+    download_urls,
+    playlist_not_supported,
+)
+
+
+__all__ = ['videomega_download']
+site_info = 'Videomega.tv'
+
+
+def videomega_download(url, info_only=False, **kwargs):
     # Hot-plug cookie handler
     ssl_context = request.HTTPSHandler(
-        context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+        context=ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    )
     cookie_handler = request.HTTPCookieProcessor()
     opener = request.build_opener(ssl_context, cookie_handler)
-    opener.addheaders = [('Referer', url),
-                         ('Cookie', 'noadvtday=0')]
+    opener.addheaders = [('Referer', url), ('Cookie', 'noadvtday=0')]
     request.install_opener(opener)
 
     if re.search(r'view\.php', url):
         php_url = url
     else:
         content = get_content(url)
-        m = re.search(r'ref="([^"]*)";\s*width="([^"]*)";\s*height="([^"]*)"', content)
+        m = re.search(
+            r'ref="([^"]*)";\s*width="([^"]*)";\s*height="([^"]*)"', content
+        )
         ref = m.group(1)
         width, height = m.group(2), m.group(3)
-        php_url = 'http://videomega.tv/view.php?ref=%s&width=%s&height=%s' % (ref, width, height)
+        php_url = (
+            'http://videomega.tv/view.php?ref={}&width={}&height={}'.format(
+                ref, width, height
+            )
+        )
     content = get_content(php_url)
 
     title = match1(content, r'<title>(.*)</title>')
@@ -33,12 +52,12 @@ def videomega_download(url, output_dir='.', merge=True, info_only=False, **kwarg
     s = match1(js, r"'([^']+)'\.split").split('|')
     src = t.format(*s)
 
-    type, ext, size = url_info(src, faker=True)
+    _type, ext, size = url_info(src, faker=True)
 
-    print_info(site_info, title, type, size)
+    print_info(site_info, title, _type, size)
     if not info_only:
-        download_urls([src], title, ext, size, output_dir, merge=merge, faker=True)
+        download_urls([src], title, ext, size, **kwargs)
 
-site_info = "Videomega.tv"
+
 download = videomega_download
-download_playlist = playlist_not_supported('videomega')
+download_playlist = playlist_not_supported(site_info)
