ec7689e14e0a67357af01af0febcd42611d4c087
iawia002 <z2d@jifangcheng.com>
2018-02-14 20:24:14 +0800

    extractors/wanmen: code format

--- lulu/extractors/wanmen.py
+++ lulu/extractors/wanmen.py
@@ -1,98 +1,121 @@
 #!/usr/bin/env python
 
-__all__ = ['wanmen_download', 'wanmen_download_by_course', 'wanmen_download_by_course_topic', 'wanmen_download_by_course_topic_part']
+import json
 
-from ..common import *
-from .bokecc import bokecc_download_by_id
-from json import loads
+from lulu.common import (
+    match1,
+    get_content,
+)
+from lulu.util import log
+from lulu.extractors.bokecc import bokecc_download_by_id
 
 
-##Helper functions
+__all__ = [
+    'wanmen_download', 'wanmen_download_by_course',
+    'wanmen_download_by_course_topic',
+    'wanmen_download_by_course_topic_part'
+]
+site_info = '万门 wanmen.org'
+
+
+# Helper functions
 def _wanmen_get_json_api_content_by_courseID(courseID):
     """int->JSON
-    
-    Return a parsed JSON tree of WanMen's API."""
 
-    return loads(get_content('http://api.wanmen.org/course/getCourseNested/{courseID}'.format(courseID = courseID)))
+    Return a parsed JSON tree of WanMen's API.
+    """
+
+    return json.loads(get_content(
+        'http://api.wanmen.org/course/getCourseNested/{courseID}'.format(
+            courseID=courseID)
+        ))
+
 
 def _wanmen_get_title_by_json_topic_part(json_content, tIndex, pIndex):
     """JSON, int, int, int->str
-    
-    Get a proper title with courseid+topicID+partID."""
 
-    return '_'.join([json_content[0]['name'],
-                    json_content[0]['Topics'][tIndex]['name'],
-                    json_content[0]['Topics'][tIndex]['Parts'][pIndex]['name']])
+    Get a proper title with courseid+topicID+partID.
+    """
+
+    return '_'.join([
+        json_content[0]['name'],
+        json_content[0]['Topics'][tIndex]['name'],
+        json_content[0]['Topics'][tIndex]['Parts'][pIndex]['name']
+    ])
 
 
 def _wanmen_get_boke_id_by_json_topic_part(json_content, tIndex, pIndex):
     """JSON, int, int, int->str
-    
+
     Get one BokeCC video ID with courseid+topicID+partID."""
 
     return json_content[0]['Topics'][tIndex]['Parts'][pIndex]['ccVideoLink']
 
 
-##Parsers
-def wanmen_download_by_course(json_api_content, output_dir='.', merge=True, info_only=False, **kwargs):
+# Parsers
+def wanmen_download_by_course(json_api_content, info_only=False, **kwargs):
     """int->None
-    
+
     Download a WHOLE course.
-    Reuse the API call to save time."""
+    Reuse the API call to save time.
+    """
 
     for tIndex in range(len(json_api_content[0]['Topics'])):
-        for pIndex in range(len(json_api_content[0]['Topics'][tIndex]['Parts'])):
-            wanmen_download_by_course_topic_part(json_api_content,
-                                                 tIndex,
-                                                 pIndex,
-                                                 output_dir=output_dir,
-                                                 merge=merge,
-                                                 info_only=info_only,
-                                                 **kwargs)
+        for pIndex in range(
+            len(json_api_content[0]['Topics'][tIndex]['Parts'])
+        ):
+            wanmen_download_by_course_topic_part(
+                json_api_content, tIndex, pIndex, info_only=info_only,
+                **kwargs
+            )
 
 
-def wanmen_download_by_course_topic(json_api_content, tIndex, output_dir='.', merge=True, info_only=False, **kwargs):
+def wanmen_download_by_course_topic(
+    json_api_content, tIndex, info_only=False, **kwargs
+):
     """int, int->None
-    
+
     Download a TOPIC of a course.
-    Reuse the API call to save time."""
+    Reuse the API call to save time.
+    """
 
     for pIndex in range(len(json_api_content[0]['Topics'][tIndex]['Parts'])):
-        wanmen_download_by_course_topic_part(json_api_content,
-                                             tIndex,
-                                             pIndex, 
-                                            output_dir=output_dir,
-                                            merge=merge,
-                                            info_only=info_only,
-                                            **kwargs)
+        wanmen_download_by_course_topic_part(
+            json_api_content, tIndex, pIndex, info_only=info_only, **kwargs
+        )
 
-def wanmen_download_by_course_topic_part(json_api_content, tIndex, pIndex, output_dir='.', merge=True, info_only=False, **kwargs):
+
+def wanmen_download_by_course_topic_part(
+    json_api_content, tIndex, pIndex, info_only=False, **kwargs
+):
     """int, int, int->None
-    
-    Download ONE PART of the course."""
+
+    Download ONE PART of the course.
+    """
 
     html = json_api_content
 
-    title = _wanmen_get_title_by_json_topic_part(html, 
-                                                  tIndex, 
-                                                  pIndex)
+    title = _wanmen_get_title_by_json_topic_part(
+        html, tIndex, pIndex
+    )
+    bokeccID = _wanmen_get_boke_id_by_json_topic_part(
+        html, tIndex, pIndex
+    )
+    bokecc_download_by_id(
+        vid=bokeccID, title=title, info_only=info_only, **kwargs
+    )
 
-    bokeccID = _wanmen_get_boke_id_by_json_topic_part(html,
-                                                      tIndex, 
-                                                     pIndex)
 
-    bokecc_download_by_id(vid = bokeccID, title = title, output_dir=output_dir, merge=merge, info_only=info_only, **kwargs)
+# Main entrance
+def wanmen_download(url, info_only=False, **kwargs):
 
-
-##Main entrance
-def wanmen_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
-
-    if not 'wanmen.org' in url:
-        log.wtf('You are at the wrong place dude. This is for WanMen University!')
-        raise
+    if 'wanmen.org' not in url:
+        log.wtf(
+            'You are at the wrong place dude. This is for WanMen University!'
+        )
 
     courseID = int(match1(url, r'course\/(\d+)'))
-    assert courseID > 0  #without courseID we cannot do anything
+    assert courseID > 0  # without courseID we cannot do anything
 
     tIndex = int(match1(url, r'tIndex=(\d+)'))
 
@@ -100,24 +123,20 @@ def wanmen_download(url, output_dir='.', merge=True, info_only=False, **kwargs):
 
     json_api_content = _wanmen_get_json_api_content_by_courseID(courseID)
 
-    if pIndex:  #only download ONE single part
+    if pIndex:  # only download ONE single part
         assert tIndex >= 0
-        wanmen_download_by_course_topic_part(json_api_content, tIndex, pIndex, 
-                                            output_dir = output_dir, 
-                                            merge = merge, 
-                                            info_only = info_only)
-    elif tIndex:  #download a topic
-        wanmen_download_by_course_topic(json_api_content, tIndex, 
-                                       output_dir = output_dir, 
-                                       merge = merge, 
-                                       info_only = info_only)
-    else:  #download the whole course
-        wanmen_download_by_course(json_api_content,
-                                 output_dir = output_dir, 
-                                 merge = merge, 
-                                 info_only = info_only)
+        wanmen_download_by_course_topic_part(
+            json_api_content, tIndex, pIndex, info_only=info_only, **kwargs
+        )
+    elif tIndex:  # download a topic
+        wanmen_download_by_course_topic(
+            json_api_content, tIndex, info_only=info_only, **kwargs
+        )
+    else:  # download the whole course
+        wanmen_download_by_course(
+            json_api_content, info_only=info_only, **kwargs
+        )
 
 
-site_info = "WanMen University"
 download = wanmen_download
 download_playlist = wanmen_download_by_course
