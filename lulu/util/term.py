ee0f2763d6c11086cb6b207426fa8004bc50c570
iawia002 <z2d@jifangcheng.com>
2018-02-18 20:30:22 +0800

    utils: code format

--- lulu/util/term.py
+++ lulu/util/term.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
+
 def get_terminal_size():
     """Get (width, height) of the current terminal."""
     try:
-        import fcntl, termios, struct # fcntl module only available on Unix
+        # fcntl module only available on Unix
+        import fcntl
+        import termios
+        import struct
         return struct.unpack('hh', fcntl.ioctl(1, termios.TIOCGWINSZ, '1234'))
-    except:
+    except Exception:
         return (40, 80)
