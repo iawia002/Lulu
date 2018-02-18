a7c2ac947150140711e7a4d1127fc585253fb6ea
iawia002 <z2d@jifangcheng.com>
2018-02-18 21:12:22 +0800

    processor: code format

--- lulu/processor/join_flv.py
+++ lulu/processor/join_flv.py
@@ -28,16 +28,20 @@ AMF_TYPE_XML = 0x0F
 AMF_TYPE_CLASS_OBJECT = 0x10
 AMF_TYPE_AMF3_OBJECT = 0x11
 
+
 class ECMAObject:
     def __init__(self, max_number):
         self.max_number = max_number
         self.data = []
         self.map = {}
+
     def put(self, k, v):
         self.data.append((k, v))
         self.map[k] = v
+
     def get(self, k):
         return self.map[k]
+
     def set(self, k, v):
         for i in range(len(self.data)):
             if self.data[i][0] == k:
@@ -46,20 +50,26 @@ class ECMAObject:
         else:
             raise KeyError(k)
         self.map[k] = v
+
     def keys(self):
         return self.map.keys()
+
     def __str__(self):
         return 'ECMAObject<' + repr(self.map) + '>'
+
     def __eq__(self, other):
         return self.max_number == other.max_number and self.data == other.data
 
+
 def read_amf_number(stream):
     return struct.unpack('>d', stream.read(8))[0]
+
 
 def read_amf_boolean(stream):
     b = read_byte(stream)
     assert b in (0, 1)
     return bool(b)
+
 
 def read_amf_string(stream):
     xx = stream.read(2)
@@ -71,6 +81,7 @@ def read_amf_string(stream):
     assert len(s) == n
     return s.decode('utf-8')
 
+
 def read_amf_object(stream):
     obj = {}
     while True:
@@ -81,6 +92,7 @@ def read_amf_object(stream):
         v = read_amf(stream)
         obj[k] = v
     return obj
+
 
 def read_amf_mixed_array(stream):
     max_number = read_uint(stream)
@@ -98,12 +110,14 @@ def read_amf_mixed_array(stream):
     assert len(mixed_results.data) == max_number
     return mixed_results
 
+
 def read_amf_array(stream):
     n = read_uint(stream)
     v = []
     for i in range(n):
         v.append(read_amf(stream))
     return v
+
 
 amf_readers = {
     AMF_TYPE_NUMBER: read_amf_number,
@@ -114,11 +128,14 @@ amf_readers = {
     AMF_TYPE_ARRAY: read_amf_array,
 }
 
+
 def read_amf(stream):
     return amf_readers[read_byte(stream)](stream)
 
+
 def write_amf_number(stream, v):
     stream.write(struct.pack('>d', v))
+
 
 def write_amf_boolean(stream, v):
     if v:
@@ -126,10 +143,12 @@ def write_amf_boolean(stream, v):
     else:
         stream.write(b'\x00')
 
+
 def write_amf_string(stream, s):
     s = s.encode('utf-8')
     stream.write(struct.pack('>H', len(s)))
     stream.write(s)
+
 
 def write_amf_object(stream, o):
     for k in o:
@@ -137,6 +156,7 @@ def write_amf_object(stream, o):
         write_amf(stream, o[k])
     write_amf_string(stream, '')
     write_byte(stream, AMF_TYPE_END_OF_OBJECT)
+
 
 def write_amf_mixed_array(stream, o):
     write_uint(stream, o.max_number)
@@ -146,10 +166,12 @@ def write_amf_mixed_array(stream, o):
     write_amf_string(stream, '')
     write_byte(stream, AMF_TYPE_END_OF_OBJECT)
 
+
 def write_amf_array(stream, o):
     write_uint(stream, len(o))
     for v in o:
         write_amf(stream, v)
+
 
 amf_writers_tags = {
     float: AMF_TYPE_NUMBER,
@@ -169,6 +191,7 @@ amf_writers = {
     AMF_TYPE_ARRAY: write_amf_array,
 }
 
+
 def write_amf(stream, v):
     if isinstance(v, ECMAObject):
         tag = amf_writers_tags[ECMAObject]
@@ -177,28 +200,36 @@ def write_amf(stream, v):
     write_byte(stream, tag)
     amf_writers[tag](stream, v)
 
+
 ##################################################
 # FLV
 ##################################################
 
+
 def read_int(stream):
     return struct.unpack('>i', stream.read(4))[0]
+
 
 def read_uint(stream):
     return struct.unpack('>I', stream.read(4))[0]
 
+
 def write_uint(stream, n):
     stream.write(struct.pack('>I', n))
+
 
 def read_byte(stream):
     return ord(stream.read(1))
 
+
 def write_byte(stream, b):
     stream.write(bytes([b]))
+
 
 def read_unsigned_medium_int(stream):
     x1, x2, x3 = struct.unpack('BBB', stream.read(3))
     return (x1 << 16) | (x2 << 8) | x3
+
 
 def read_tag(stream):
     # header size: 15 bytes
@@ -215,29 +246,22 @@ def read_tag(stream):
     assert x[9:] == (0, 0, 0)
     body = stream.read(body_size)
     return (data_type, timestamp, body_size, body, previous_tag_size)
-    #previous_tag_size = read_uint(stream)
-    #data_type = read_byte(stream)
-    #body_size = read_unsigned_medium_int(stream)
-    #assert body_size < 1024*1024*128, 'tag body size too big (> 128MB)'
-    #timestamp = read_unsigned_medium_int(stream)
-    #timestamp += read_byte(stream) << 24
-    #assert read_unsigned_medium_int(stream) == 0
-    #body = stream.read(body_size)
-    #return (data_type, timestamp, body_size, body, previous_tag_size)
+
 
 def write_tag(stream, tag):
     data_type, timestamp, body_size, body, previous_tag_size = tag
     write_uint(stream, previous_tag_size)
     write_byte(stream, data_type)
-    write_byte(stream, body_size>>16 & 0xff)
-    write_byte(stream, body_size>>8  & 0xff)
-    write_byte(stream, body_size     & 0xff)
-    write_byte(stream, timestamp>>16 & 0xff)
-    write_byte(stream, timestamp>>8  & 0xff)
-    write_byte(stream, timestamp     & 0xff)
-    write_byte(stream, timestamp>>24 & 0xff)
+    write_byte(stream, body_size >> 16 & 0xff)
+    write_byte(stream, body_size >> 8 & 0xff)
+    write_byte(stream, body_size & 0xff)
+    write_byte(stream, timestamp >> 16 & 0xff)
+    write_byte(stream, timestamp >> 8 & 0xff)
+    write_byte(stream, timestamp & 0xff)
+    write_byte(stream, timestamp >> 24 & 0xff)
     stream.write(b'\0\0\0')
     stream.write(body)
+
 
 def read_flv_header(stream):
     assert stream.read(3) == b'FLV'
@@ -248,16 +272,19 @@ def read_flv_header(stream):
     data_offset = read_uint(stream)
     assert data_offset == 9
 
+
 def write_flv_header(stream):
     stream.write(b'FLV')
     write_byte(stream, 1)
     write_byte(stream, 5)
     write_uint(stream, 9)
 
+
 def read_meta_data(stream):
     meta_type = read_amf(stream)
     meta = read_amf(stream)
     return meta_type, meta
+
 
 def read_meta_tag(tag):
     data_type, timestamp, body_size, body, previous_tag_size = tag
@@ -266,10 +293,6 @@ def read_meta_tag(tag):
     assert previous_tag_size == 0
     return read_meta_data(BytesIO(body))
 
-#def write_meta_data(stream, meta_type, meta_data):
-#    assert isinstance(meta_type, basesting)
-#    write_amf(meta_type)
-#    write_amf(meta_data)
 
 def write_meta_tag(stream, meta_type, meta_data):
     buffer = BytesIO()
@@ -283,6 +306,7 @@ def write_meta_tag(stream, meta_type, meta_data):
 # main
 ##################################################
 
+
 def guess_output(inputs):
     import os.path
     inputs = map(os.path.basename, inputs)
@@ -292,14 +316,15 @@ def guess_output(inputs):
             return inputs[0][:i] + '.flv'
     return 'output.flv'
 
-def concat_flv(flvs, output = None):
+
+def concat_flv(flvs, output=None):
     assert flvs, 'no flv file found'
     import os.path
     if not output:
         output = guess_output(flvs)
     elif os.path.isdir(output):
         output = os.path.join(output, guess_output(flvs))
-    
+
     print('Merging video parts...')
     ins = [open(flv, 'rb') for flv in flvs]
     for stream in ins:
@@ -309,13 +334,13 @@ def concat_flv(flvs, output = None):
     meta_types, metas = zip(*metas)
     assert len(set(meta_types)) == 1
     meta_type = meta_types[0]
-    
+
     # must merge fields: duration
     # TODO: check other meta info, update other meta info
     total_duration = sum(meta.get('duration') for meta in metas)
     meta_data = metas[0]
     meta_data.set('duration', total_duration)
-    
+
     out = open(output, 'wb')
     write_flv_header(out)
     write_meta_tag(out, meta_type, meta_data)
@@ -332,25 +357,28 @@ def concat_flv(flvs, output = None):
                 break
         timestamp_start = timestamp
     write_uint(out, previous_tag_size)
-    
+
     return output
+
 
 def usage():
     print('Usage: [python3] join_flv.py --output TARGET.flv flv...')
 
+
 def main():
-    import sys, getopt
+    import sys
+    import getopt
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], 'ho:', ['help', 'output='])
     except getopt.GetoptError as err:
         usage()
         sys.exit(1)
     output = None
     for o, a in opts:
-        if o in ("-h", "--help"):
+        if o in ('-h', '--help'):
             usage()
             sys.exit()
-        elif o in ("-o", "--output"):
+        elif o in ('-o', '--output'):
             output = a
         else:
             usage()
@@ -358,8 +386,9 @@ def main():
     if not args:
         usage()
         sys.exit(1)
-    
+
     concat_flv(args, output)
+
 
 if __name__ == '__main__':
     main()
--- lulu/processor/join_ts.py
+++ lulu/processor/join_ts.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
-import struct
-from io import BytesIO
 
 ##################################################
 # main
 ##################################################
+
 
 def guess_output(inputs):
     import os.path
@@ -16,16 +15,17 @@ def guess_output(inputs):
             return inputs[0][:i] + '.ts'
     return 'output.ts'
 
-def concat_ts(ts_parts, output = None):
+
+def concat_ts(ts_parts, output=None):
     assert ts_parts, 'no ts files found'
     import os.path
     if not output:
         output = guess_output(ts_parts)
     elif os.path.isdir(output):
         output = os.path.join(output, guess_output(ts_parts))
-    
+
     print('Merging video parts...')
-    
+
     ts_out_file = open(output, "wb")
     for ts_in in ts_parts:
         ts_in_file = open(ts_in, "rb")
@@ -35,22 +35,25 @@ def concat_ts(ts_parts, output = None):
     ts_out_file.close()
     return output
 
+
 def usage():
     print('Usage: [python3] join_ts.py --output TARGET.ts ts...')
 
+
 def main():
-    import sys, getopt
+    import sys
+    import getopt
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ho:", ["help", "output="])
+        opts, args = getopt.getopt(sys.argv[1:], 'ho:', ['help', 'output='])
     except getopt.GetoptError as err:
         usage()
         sys.exit(1)
     output = None
     for o, a in opts:
-        if o in ("-h", "--help"):
+        if o in ('-h', '--help'):
             usage()
             sys.exit()
-        elif o in ("-o", "--output"):
+        elif o in ('-o', '--output'):
             output = a
         else:
             usage()
@@ -58,8 +61,9 @@ def main():
     if not args:
         usage()
         sys.exit(1)
-    
+
     concat_ts(args, output)
+
 
 if __name__ == '__main__':
     main()
